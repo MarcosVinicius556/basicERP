8361f533abc522c037b41fc3e811cb68a844aef4
marcos <marcosvinicios4132@gmail.com>
2024-02-15 09:54:10 -0300

    Tela de financeiro colocada nos padrões das demais views

--- frontend/src/views/Finance/index.js
+++ frontend/src/views/Finance/index.js
@@ -8,6 +8,7 @@ import {
   Card,
   CardHeader,
   CardBody,
+  Button,
 } from "reactstrap";
 
 const Finance = () => {
@@ -19,16 +20,75 @@ const Finance = () => {
           <Col className="order-xl-1" xl="8">
             <Card>
             <CardHeader>
-              <h1>Aqui será a página do financeiro</h1>
+              <Row className="align-items-center">
+                <Col xs="6">
+                  <h1>Financeiro</h1>      
+                </Col>
+              </Row>
             </CardHeader>
             <CardBody>
-              <p>lorem ipsun</p>
+              <h6 className="heading-small text-muted mb-4">
+                    Relatórios
+                  </h6>
+              <div className="pl-lg-4">
+                <Row className="align-items-center mb-3">
+                  <Col xs="6">
+                    <Button
+                        color="primary"
+                        onClick={(e) => e.preventDefault()}
+                        size="md"
+                      >
+                        Relatório de OS em aberto
+                    </Button>
+                  </Col>
+                  <Col xs="6">
+                    <Button
+                        color="primary"
+                        onClick={(e) => e.preventDefault()}
+                        size="md"
+                    >
+                      Relatório de OS em andamento
+                    </Button>
+                  </Col>
+                  </Row>
+                  <Row className="align-items-center">
+                  <Col xs="6">
+                    <Button
+                      color="primary"
+                      onClick={(e) => e.preventDefault()}
+                      size="md">
+                      Relatório de OS finalizada
+                    </Button>
+                  </Col>
+                  <Col xs="6">
+                    <Button
+                      color="primary"
+                      onClick={(e) => e.preventDefault()}
+                      size="md">
+                      Relatório de OS finalizada
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+              <hr className="my-4" />
+
+              <h6 className="heading-small text-muted mb-4">
+                    Outras Opcçoes...
+                  </h6>
+              <div className="pl-lg-4">
+              <Row className="align-items-center mb-3">
+                <Col xs="6">
+                  <h5>Avaliar as opções que seriam necessárias</h5>
+                </Col>
+              </Row>
+              </div>
+
             </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  </>
   );
 };
 
